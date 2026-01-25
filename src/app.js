7c31cb0afa5812ce4d2a9c00835421e3835c20e4
bikash-kumar-kc <kcbikash886@gmail.com>
2026-01-25 17:35:13 +0545

    add routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import express from "express";
+import AuthRoutes from "../src/resources/authResources/auth.routes.js";
+import UserRoutes from "./resources/userResources/user.routes.js";
+import SubscriptionRoutes from "./resources/subscriptionResources/subscription.routes.js";
 
 const app = express();
 
 
+app.use(express.json());
+
+
+// HOME ROUTE
 app.get("/",(res)=>{
    return res.send("Welcome to subscription management system !!");
 });
+
+// ROUTES
+
+app.use("/api/v1/auth",AuthRoutes);
+app.use("/api/v1/users",UserRoutes);
+app.use("/api/v1/subscriptions",SubscriptionRoutes);
+
+
+
+
+
 
 
 export default app;
