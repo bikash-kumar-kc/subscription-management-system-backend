8e0b69bce32a09dc75ebc364e59383412438ecff
bikash-kumar-kc <kcbikash886@gmail.com>
2026-01-25 18:16:14 +0545

    improve route conflict

--- src/resources/subscriptionResources/subscription.routes.js
+++ src/resources/subscriptionResources/subscription.routes.js
@@ -5,26 +5,26 @@ const SubscriptionRoutes = express.Router();
 SubscriptionRoutes.get("/", async (req, res) =>
   res.send({ message: "Get all subscriptions" }),
 );
-SubscriptionRoutes.get("/:id", async (req, res) =>
-  res.send({ message: "Get a subscription" }),
-);
+
 SubscriptionRoutes.post("/", async (req, res) =>
   res.send({ message: "Create a subscription" }),
 );
-SubscriptionRoutes.put("/:id", async (req, res) =>
-  res.send({ message: "Update a subscription" }),
-);
+
 SubscriptionRoutes.delete("/:id", async (req, res) =>
   res.send({ message: "Delete a subscription" }),
 );
 SubscriptionRoutes.get("/user/:id", async (req, res) =>
   res.send({ message: "Get subscription of a user" }),
 );
-SubscriptionRoutes.put("/user/cancel", async (req, res) =>
+SubscriptionRoutes.put("/user/cancel/:id", async (req, res) =>
   res.send({ message: "Cancel a subscription" }),
 );
 SubscriptionRoutes.get("/upcomming-renewals", async (req, res) =>
   res.send({ message: "Get all upcomming renewels" }),
+);
+
+SubscriptionRoutes.get("/:id", async (req, res) =>
+  res.send({ message: "Get a subscription" }),
 );
 
 export default SubscriptionRoutes;
