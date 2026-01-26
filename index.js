5142972071dca539fb1aa6158cc08fc90d7b5d34
bikash-kumar-kc <kcbikash886@gmail.com>
2026-01-26 15:33:51 +0545

    connect mongodb

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import app from "./src/app.js";
 import { config } from "./src/config/config.js";
+import connectToMongoDB from "./src/database/mongoDB.js";
 
 const startServer = async () => {
+
+
+  await connectToMongoDB();
   app.listen(config.PORT, () => {
     console.log(`the server is running at PORT:: ${config.PORT}`);
   });
