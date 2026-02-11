ffc5846a8825604fa0f2148487f6d3ae81a722ab
bikash-kumar-kc <kcbikash886@gmail.com>
2026-02-11 14:41:05 +0545

    create log file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import path from "node:path";
+import arcjet from "./middleware/arcjet.middleware.js";
+import { fileURLToPath } from "node:url";
+import morgan from "morgan";
+import fs from "node:fs";
+
+import workflowRouter from "./resources/workflow/workflow.routes.js";
+import { stripHook } from "./stripe/stripe.controller.js";
 import AuthRoutes from "../src/resources/authResources/auth.routes.js";
 import UserRoutes from "./resources/userResources/user.routes.js";
 import SubscriptionRoutes from "./resources/subscriptionResources/subscription.routes.js";
 import globalErrorHandler from "./middleware/globalErrorHandler.js";
-import cookieParser from "cookie-parser";
-// import arcjet from "./middleware/arcjet.middleware.js";
-import workflowRouter from "./resources/workflow/workflow.routes.js";
-import { stripHook } from "./stripe/stripe.controller.js";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import stripeRouter from "./stripe/stripe.route.js";
 
 const app = express();
@@ -17,8 +20,27 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// MAINTAINING REQUEST AND ERROR LOG
+const logsDir = path.join(__dirname, "logs");
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
 
+// CREATE REQUEST LOGS FILE
+const requestLogsStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "requests.log"),
+  {
+    flags: "a",
+  },
+);
 
+// CREATE ERROR LOGS FILE
+const errorLogs = fs.createWriteStream(
+  path.join(__dirname, "logs", "errorLogs"),
+  {
+    flags: "a",
+  },
+);
 
 app.post(
   "/stripe-webhook",
@@ -29,10 +51,16 @@ app.post(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(arcjet);
+app.use(arcjet);
 app.use(
   "/static_files",
   express.static(path.join(__dirname, "../public/static_files")),
+);
+
+app.use(
+  morgan("combined", {
+    stream: requestLogsStream,
+  }),
 );
 
 // HOME ROUTE
@@ -44,11 +72,18 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/users", UserRoutes);
 app.use("/api/v1/subscriptions", SubscriptionRoutes);
-app.use("/payment",stripeRouter)
-
+app.use("/payment", stripeRouter);
 app.use("/api/v1/workflows", workflowRouter);
 
 // GLOBAL ERROR HANDLER
 app.use(globalErrorHandler);
+app.use(
+  morgan("combined", {
+    stream: errorLogs,
+    skip: function (req, res) {
+      return res.statusCode < 400;
+    },
+  }),
+);
 
 export default app;
