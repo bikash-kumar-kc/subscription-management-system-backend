87d688fab73f08663ae94236e03500a48ea54a6d
bikash-kumar-kc <kcbikash886@gmail.com>
2026-02-13 15:08:41 +0545

    using vercel crons job

--- src/services/cron.js
+++ src/services/cron.js
@@ -1,9 +1,7 @@
-import { CronJob } from "cron";
 import Subscription from "../resources/subscriptionResources/subscription.model.js";
-
-const startCronJobs = new CronJob(
-  " 0 * * * * ",
-  async () => {
+import config from "../config/config.js";
+const cronsJobController = async (req, res) => {
+  try {
     const result_1 = await Subscription.updateMany(
       {
         status: "active",
@@ -33,14 +31,23 @@ const startCronJobs = new CronJob(
           },
         },
       ],
-      { updatePipeline: true }
+      { updatePipeline: true },
     );
 
-    console.log(`Marked ${result_1.modifiedCount} Subscription as expired`);
-    console.log(`Marked ${result_2.modifiedCount} Subscription as can renew`);
-  },
-  () => console.log("status is checked!!!"),
-  false,
-);
+    console.log(`Expired: ${result_1.modifiedCount}`);
+    console.log(`Can Renew: ${result_2.modifiedCount}`);
 
-export default startCronJobs;
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    const isProduction = config.NODE_ENV === "production" ? true : false;
+    const error = {
+      message: isProduction ? "problem in crons job" : err.message,
+      stack: isProduction ? undefined : err.stack,
+      statusCode: err.statusCode || 500,
+    };
+
+    return res.status(error.statusCode).json({ error: error });
+  }
+};
+
+export default cronsJobController;
